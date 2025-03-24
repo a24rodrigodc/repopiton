13226d90f79877603a070e810b089f13b2daa842
a24rodrigodc <a24rodrigodc@iessanclemente.net>
2025-03-24 19:51:48 +0100

    Create test_utils.py

---
+++ tests/test_utils.py
@@ -0,0 +1,10 @@
+import pytest
+from utils.utils import suma, resta
+
+def test_suma():
+    assert suma(2, 3) == 5
+    assert suma(-1, 1) == 0
+
+def test_resta():
+    assert resta(5, 2) == 3
+    assert resta(10, 10) == 0
